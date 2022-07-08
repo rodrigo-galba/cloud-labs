2551b92b38ce490b3e8b08598a25bacf55aa54c0
Rodrigo Galba <rodrigogalba@gmail.com>
2022-07-08 11:53:09 -0300

    Rename test

--- python/python-scripts-course/sum_test.py
+++ python/python-scripts-course/sum_test.py
@@ -3,7 +3,8 @@ from model import operations
 
 
 class TestSum(unittest.TestCase):
-    def test_sum(self):
+
+    def test_sum_operation(self):
         self.assertEqual(operations.sum_models(1, 2), 3, "should be equal")
         self.assertEqual(operations.sum_models(3, 4), 7, "should be equal")
         self.assertEqual(operations.sum_models(5, 6), 11, "should be equal")
