22c1403f7926e91fdc073cf0e6f58b40bb64c63a
Rodrigo Galba <rodrigogalba@gmail.com>
2022-07-08 11:52:24 -0300

    Add fastapi debugger for pycharm

--- python/fast-my-api/main.py
+++ python/fast-my-api/main.py
@@ -1,4 +1,5 @@
 import os
+import uvicorn
 
 from fastapi import FastAPI
 from pydantic import BaseModel
@@ -7,7 +8,7 @@ from mangum import Mangum
 STAGE = os.environ.get('STAGE', None)
 openapi_prefix = "/" if not STAGE else f'/{STAGE}'
 
-app = FastAPI(title="MyAwesomeApp", openapi_prefix=openapi_prefix)
+app = FastAPI(title="MyAwesomeApp", root_path=openapi_prefix)
 
 # Mangum Handler, this is so important
 handler = Mangum(app)
@@ -49,3 +50,6 @@ def create_user(user: User):
     # TODO validate request before append
     users_list.append(user)
     return user
+
+if __name__ == '__main__':
+    uvicorn.run(app, host="0.0.0.0", port=8000)
