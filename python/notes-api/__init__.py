563965af193a6a40152083d73801d50a9640804f
Rodrigo Galba <rodrigogalba@gmail.com>
2022-08-22 00:16:14 -0300

    Create initial models and migrate database

--- python/notes-api/__init__.py
+++ python/notes-api/__init__.py
@@ -2,6 +2,7 @@ from email.policy import default
 import os
 
 from flask import Flask
+from flask_migrate import Migrate
 
 def create_app(test_config=None):
     app = Flask(__name__)
@@ -13,5 +14,10 @@ def create_app(test_config=None):
         app.config.from_pyfile('config.py', silent=True)
     else:
         app.config.from_mapping(test_config)
+
+    from .models import db
+
+    db.init_app(app)
+    migrate = Migrate(app, db)
 
     return app
---
+++ python/notes-api/models.py
@@ -0,0 +1,20 @@
+from enum import unique
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(200))
+    create_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+    notes = db.relationship('Note', backref='author', lazy=True)
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200))
+    body = db.Column(db.Text)
+    create_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+    author = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
